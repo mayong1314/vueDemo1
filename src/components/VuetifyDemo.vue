0d250421d4c0e9b5878d3c09f355c4e3bba0c963
mayong <2772471646@qq.com>
2021-01-11 09:37:22 +0800

    提交master

--- src/components/VuetifyDemo.vue
+++ src/components/VuetifyDemo.vue
@@ -35,14 +35,14 @@
       <template slot-scope="scope">
         <el-button
           size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          @click="handleEdit(scope.$index, scope.row)">编辑此行</el-button>
         <el-button
           size="mini"
           type="danger"
           @click="handleDelete(scope.$index, scope.row)">删除</el-button>
       </template>
     </el-table-column>
-    
+
   </el-table>
   <el-pagination
   background
